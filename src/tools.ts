fb80d6af3b3fbea0cebe1a23e0fc0797a5845f9e
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-16 15:48:23 +0100

    with docu tools

--- src/tools.ts
+++ src/tools.ts
@@ -1,5 +1,11 @@
 import { IJob, ISkill } from './interfaces';
 
+/**
+ * expandSkillsInJobs fills skills property from skillList
+ * 
+ * example: expandSkillsInJobs(jobs1, skills1);
+ * 
+ */
 export const expandSkillsInJobs = (jobs: IJob[], skills: ISkill[]) => {
 	console.log(jobs.length, skills.length)
 };
