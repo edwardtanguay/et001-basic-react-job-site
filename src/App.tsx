ad9c5154e6758e26011fb1ae1fba8c88f42c6976
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-16 14:08:17 +0100

    getting jobs in useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useEffect } from 'react';
 import './App.scss';
 
-function App() {
+const jobUrl = 'https://edwardtanguay.vercel.app/share/jobs.json';
 
-  return <div className="App">
-    <h2>Job Site</h2>
-    <p>This is the job site.</p>
-  </div>;
+function App() {
+	useEffect(() => {
+		(async () => {
+      const response = await fetch(jobUrl);
+      const jobs = await response.json();
+		})();
+	}, []);
+
+	return (
+		<div className="App">
+			<h2>Job Site</h2>
+			<p>This is the job site.</p>
+		</div>
+	);
 }
 
 export default App;
