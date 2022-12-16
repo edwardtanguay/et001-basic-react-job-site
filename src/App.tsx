e1f5abb592906327587ec681ea045cdb326df056
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-16 14:54:57 +0100

    jobs styed

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,18 @@ import './App.scss';
 
 const jobUrl = 'https://edwardtanguay.vercel.app/share/jobs.json';
 
+interface IJob {
+	id: number;
+	title: string;
+	url: string;
+	company: string;
+	description: string;
+	skillList: string;
+	publicationDate: string;
+}
+
 function App() {
-	const [jobs, setJobs] = useState([]);
+	const [jobs, setJobs] = useState<IJob[]>([]);
 
 	useEffect(() => {
 		(async () => {
@@ -18,6 +28,22 @@ function App() {
 		<div className="App">
 			<h2>Job Site</h2>
 			<p>There are {jobs.length} jobs.</p>
+			<div className="jobs">
+				{jobs.map((job) => {
+					return (
+						<div key={job.id} className="job">
+							<div className="title">
+								<a href={job.url} target="_blank">
+									{job.title}
+								</a>
+							</div>
+							<div className="company">{job.company}</div>
+							<div className="publicationDate">{job.publicationDate}</div>
+							<div className="skillList">{job.skillList}</div>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
