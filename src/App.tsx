0b46e600d5e29df219d27a716ccd0a83028c4b16
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-16 19:58:07 +0100

    loading graphic

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.scss';
 import axios from 'axios';
 import { IJob, ISkill } from './interfaces';
 import * as tools from './tools';
+import { FaSpinner } from 'react-icons/fa';
+import { FiLoader } from 'react-icons/fi';
 
 const jobUrl = 'https://edwardtanguay.vercel.app/share/jobs.json';
 const skillsUrl = 'https://edwardtanguay.vercel.app/share/skills.json';
@@ -12,101 +14,132 @@ function App() {
 	const [skills, setSkills] = useState<ISkill[]>([]);
 
 	useEffect(() => {
-		(async () => {
-			// load jobs
-			const jobsResponse = await fetch(jobUrl);
-			const _jobs = await jobsResponse.json();
-			_jobs.sort(
-				(a: IJob, b: IJob) => a.publicationDate > b.publicationDate ? -1 : 1
-			);
+		setTimeout(() => {
+			(async () => {
+				const jobsResponse = await fetch(jobUrl);
+				const _jobs = await jobsResponse.json();
+				_jobs.sort((a: IJob, b: IJob) =>
+					a.publicationDate > b.publicationDate ? -1 : 1
+				);
 
-			// load skills
-			const skillsResponse = await axios.get(skillsUrl);
-			const _skills = skillsResponse.data;
+				const skillsResponse = await axios.get(skillsUrl);
+				const _skills = skillsResponse.data;
 
-			tools.expandSkillsInJobs(_jobs, _skills);
+				tools.expandSkillsInJobs(_jobs, _skills);
 
-			setJobs(_jobs);
-			setSkills(_skills);
-		})();
+				setJobs(_jobs);
+				setSkills(_skills);
+			})();
+		}, 2000);
 	}, []);
 
 	return (
-		<div className="App"> 
+		<div className="App">
 			<a id="jobs"></a>
 			<h2>Get a Job</h2>
 			<main className="content">
-				<section className="jobArea">
-					<div className="responsiveHeader">
-						<h3>There are {jobs.length} jobs:</h3>
-						<div className="skillsLink">
-							<a href="#skills">skills</a>
+				{jobs.length === 0 ? (
+					<div>
+						<h3 className="loadingMessage">
+							Loading live data...{' '}
+						</h3>
+						<div className="loadingGraphic">
+							<FiLoader className="spinner" />
 						</div>
 					</div>
-					<div className="jobs">
-						{jobs.map((job) => {
-							return (
-								<div key={job.id} className="job">
-									<div className="header">
-										<div className="title">
-											<a href={job.url} target="_blank">
-												{job.title}
-											</a>
-										</div>
-										<div className="company">
-											{job.company}
-										</div>
-										<div className="publicationDate">
-											Posted: {job.publicationDate}
-										</div>
-									</div>
-									<div className="skills">
-										{job.skills && (
-											<>
-												{job.skills.map((skill) => {
-													return (
-														<div className="skill">
-															<a
-																href={skill.url}
-																target="_blank"
-															>
-																{skill.name}
-															</a>{' '}
-															-{' '}
-															{skill.description}
-														</div>
-													);
-												})}
-											</>
-										)}
-									</div>
+				) : (
+					<>
+						<section className="jobArea">
+							<div className="responsiveHeader">
+								<h3>There are {jobs.length} jobs:</h3>
+								<div className="skillsLink">
+									<a href="#skills">skills</a>
 								</div>
-							);
-						})}
-					</div>
-				</section>
-				<section className="skillArea">
-					<a id="skills"></a>
-					<div className="responsiveHeader">
-						<h3>There are {skills.length} skills:</h3>
-						<div className="skillsLink">
-							<a href="#jobs">jobs</a>
-						</div>
-					</div>
-					<div className="skills">
-						{skills.map((skill) => {
-							return (
-								<div key={skill.idCode} className="skill">
-									<div className="name">
-										<a target="_blank" href={skill.url}>
-											{skill.name}
-										</a>
-									</div>
+							</div>
+							<div className="jobs">
+								{jobs.map((job) => {
+									return (
+										<div key={job.id} className="job">
+											<div className="header">
+												<div className="title">
+													<a
+														href={job.url}
+														target="_blank"
+													>
+														{job.title}
+													</a>
+												</div>
+												<div className="company">
+													{job.company}
+												</div>
+												<div className="publicationDate">
+													Posted:{' '}
+													{job.publicationDate}
+												</div>
+											</div>
+											<div className="skills">
+												{job.skills && (
+													<>
+														{job.skills.map(
+															(skill) => {
+																return (
+																	<div className="skill">
+																		<a
+																			href={
+																				skill.url
+																			}
+																			target="_blank"
+																		>
+																			{
+																				skill.name
+																			}
+																		</a>{' '}
+																		-{' '}
+																		{
+																			skill.description
+																		}
+																	</div>
+																);
+															}
+														)}
+													</>
+												)}
+											</div>
+										</div>
+									);
+								})}
+							</div>
+						</section>
+						<section className="skillArea">
+							<a id="skills"></a>
+							<div className="responsiveHeader">
+								<h3>There are {skills.length} skills:</h3>
+								<div className="skillsLink">
+									<a href="#jobs">jobs</a>
 								</div>
-							);
-						})}
-					</div>
-				</section>
+							</div>
+							<div className="skills">
+								{skills.map((skill) => {
+									return (
+										<div
+											key={skill.idCode}
+											className="skill"
+										>
+											<div className="name">
+												<a
+													target="_blank"
+													href={skill.url}
+												>
+													{skill.name}
+												</a>
+											</div>
+										</div>
+									);
+								})}
+							</div>
+						</section>
+					</>
+				)}
 			</main>
 		</div>
 	);
