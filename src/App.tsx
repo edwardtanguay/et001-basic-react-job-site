f5e40a6acc8bd8d3456bf9c5db0b41e382dce020
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-17 08:34:43 +0100

    added pageLoadingArea

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useContext } from 'react';
 import { AppContext } from './AppContext';
 import './App.scss';
-import { FiLoader } from 'react-icons/fi';
 import { JobsArea } from './components/JobsArea';
 import { SkillsArea } from './components/SkillsArea';
+import { PageLoadingArea } from './components/PageLoadingArea';
 
 function App() {
-	const { jobs} =
-		useContext(AppContext);
+	const { jobs } = useContext(AppContext);
 
 	return (
 		<div className="App">
@@ -15,18 +14,11 @@ function App() {
 			<h2>Get a Job</h2>
 			<main className="content">
 				{jobs.length === 0 ? (
-					<div className="loadingArea">
-						<h3 className="loadingMessage">
-							Loading live data...{' '}
-						</h3>
-						<div className="loadingGraphic">
-							<FiLoader className="spinner" />
-						</div>
-					</div>
+					<PageLoadingArea />
 				) : (
 					<>
 						<JobsArea />
-						<SkillsArea/>
+						<SkillsArea />
 					</>
 				)}
 			</main>
